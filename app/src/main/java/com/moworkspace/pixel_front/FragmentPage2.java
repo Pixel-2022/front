0344bec1306d109b7efa01a3e045ce12f4c1b2de
soosoo030 <whereru511@gmail.com>
2022-03-28 19:47:46 +0900

    ViewPager 생성

--- app/src/main/java/com/moworkspace/pixel_front/FragmentPage2.java
+++ app/src/main/java/com/moworkspace/pixel_front/FragmentPage2.java
@@ -9,17 +9,54 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class FragmentPage2 extends Fragment {
     private androidx.recyclerview.widget.LinearLayoutManager LinearLayoutManager;
     private View v;
-
+    //ViewPager 변수 선언
+    ViewPager2 vpVertical, vpHorizontal;
+    int[] images = {R.drawable.test1, R.drawable.test2,R.drawable.test3,R.drawable.test4};
+    MainAdapter adapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         v= inflater.inflate(R.layout.fragment2,container,false);
 
+        //ViewPager 관련 코드
+        vpHorizontal = v.findViewById(R.id.vp_horizontal);
+
+        adapter = new MainAdapter(images);
+
+        //Set clip padding
+        vpHorizontal.setClipToPadding(false);
+        //Set clip children
+        vpHorizontal.setClipChildren(false);
+        //Set page limit
+        vpHorizontal.setOffscreenPageLimit(3);
+        //Set default start position
+        vpHorizontal.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_NEVER);
+        //Set adapter on horizontal viewpager
+        vpHorizontal.setAdapter(adapter);
+
+        //Initialize composite page transformer
+        CompositePageTransformer transformer = new CompositePageTransformer();
+        //Add margin between page
+        transformer.addTransformer(new MarginPageTransformer(8));
+        //Increase selected page size
+        transformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float v = 1 - Math.abs(position);
+                //Set scale y
+                page.setScaleY(0.8f + v * 0.2f);
+            }
+        });
+        // Set page transform
+        vpHorizontal.setPageTransformer(transformer);
 
         return v;
     }
@@ -28,4 +65,5 @@ public class FragmentPage2 extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
     }
+
 }
