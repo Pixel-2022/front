f2f7638af7a4b52a0e3732a540676e145f42fd18
usuny0317 <usuny0317@naver.com>
2022-03-28 16:24:26 +0900

    빈 프래그먼트 페이지 3개 생성, 메인엑티비티 -> 클릭시 다른 프래그먼트로 움직이는 코드 + 백스택 제거 코드

---
+++ app/src/main/java/com/moworkspace/pixel_front/FragmentPage3.java
@@ -0,0 +1,6 @@
+package com.moworkspace.pixel_front;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentPage3 extends Fragment {
+}
--- app/src/main/java/com/moworkspace/pixel_front/MainActivity.java
+++ app/src/main/java/com/moworkspace/pixel_front/MainActivity.java
@@ -1,14 +1,71 @@
 package com.moworkspace.pixel_front;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mBottomNV;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    String tag1, tag2, tag3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void BottomNavigate(int id) {  //BottomNavigation 페이지 변경
+        String tag = String.valueOf(id);
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+        if (currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
+
+
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+
+        if (fragment == null) {
+            if (id == R.id.navigation_1) {
+                tag1=tag;
+                fragment = new FragmentPage1();
+            } else if (id == R.id.navigation_2){
+                tag2=tag;
+                fragment = new FragmentPage2();
+            }else {
+                tag3=tag;
+                fragment = new FragmentPage3();
+            }
+
+            fragmentTransaction.add(R.id.content_layout, fragment, tag);
+        } else {
+            if(fragment==fragmentManager.findFragmentByTag(tag2)) { //메인 페이지 항시 리플래쉬
+                fragmentTransaction.remove(fragment);
+                fragment = new FragmentPage2();
+                fragmentTransaction.add(R.id.content_layout, fragment, tag2);
+            }else {
+                fragmentTransaction.show(fragment);
+            }
+        }
+
+        clearBackStack();
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
+    }
+
+
+    private void clearBackStack() {
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        while (fragmentManager.getBackStackEntryCount() != 0) {
+            fragmentManager.popBackStackImmediate();
+        }
     }
 }
