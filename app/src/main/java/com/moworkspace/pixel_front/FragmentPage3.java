36c47e0da5f47fc849673ed056be9a2de6f1e0af
usuny0317 <usuny0317@naver.com>
2022-04-15 16:48:10 +0900

    로그아웃 흐름

--- app/src/main/java/com/moworkspace/pixel_front/FragmentPage3.java
+++ app/src/main/java/com/moworkspace/pixel_front/FragmentPage3.java
@@ -1,18 +1,26 @@
 package com.moworkspace.pixel_front;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 public class FragmentPage3 extends Fragment {
     private androidx.recyclerview.widget.LinearLayoutManager LinearLayoutManager;
     private View v;
+    private Context ct;
 
 
     @Override
@@ -20,6 +28,36 @@ public class FragmentPage3 extends Fragment {
         setHasOptionsMenu(true);
         v= inflater.inflate(R.layout.fragment3,container,false);
 
+        ct=container.getContext();
+        Button logout=v.findViewById(R.id.setLogout);
+        TextView notice=v.findViewById(R.id.setNotice);
+        TextView ask=v.findViewById(R.id.setAsk);
+
+        //로그아웃 시 로그인 페이지로
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //공지사항 토스트
+        notice.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ct,"공지사항.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //문의사항 토스트
+        ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ct,"문의사항이 없습니다.", Toast.LENGTH_LONG).show();
+            }
+        });
 
         return v;
     }
