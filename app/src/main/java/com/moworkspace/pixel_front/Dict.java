ac19441896a8e55caec98989dc649be1b8457822
soosoo030 <whereru511@gmail.com>
2022-05-26 22:13:27 +0900

    단어사전 영상 - Back 연동

--- app/src/main/java/com/moworkspace/pixel_front/Dict.java
+++ app/src/main/java/com/moworkspace/pixel_front/Dict.java
@@ -7,14 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Dict extends AppCompatActivity {
     String Word;
     String image;
+    String videoURL;
 
     public String getWord(){return Word;}
     public void setWord(String s){Word = s;}
     public String getImage(){return image;}
     public void setImage(String im){image=im;}
+    public String getVideoURL(){return videoURL;}
+    public void setVideoURL(String vu){videoURL=vu;}
 
-    public Dict(String Word, String image){
+    public Dict(String Word, String image,String videoURL){
         this.Word=Word;
         this.image=image;
+        this.videoURL=videoURL;
     }
 }
